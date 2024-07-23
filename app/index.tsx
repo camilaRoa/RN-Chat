098bb90eda6e61bf51772f6ae7febbdf87bfcc6b
Brihan Penayo <brihan.penayos@gmail.com>
2024-07-23 11:51:18 -0400

    feat:first-commit-using-nativewind@4

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { Text, View } from "react-native";
+
+const index = () => {
+  return (
+    <View className="flex justify-center items-center bg-slate-200 text-center h-screen">
+      <Text>Hola mundo</Text>
+    </View>
+  );
+};
+
+export default index;
